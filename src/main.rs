5a4a682da80f2cac98d11d9672de139a9c2b74e9
gifnksm <makoto.nksm+github@gmail.com>
2021-05-11 22:40:09 +0900

    dump memory map (osbook_day08a)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@
 #![no_main]
 
 use self::error::{Error, ErrorKind, Result};
-use bootloader::{boot_info::Optional, entry_point, BootInfo};
+use bootloader::{
+    boot_info::{MemoryRegion, Optional},
+    entry_point, BootInfo,
+};
 use core::mem;
 
 mod console;
@@ -16,6 +19,29 @@ mod framebuffer;
 mod graphics;
 mod mouse;
 mod pci;
+
+struct MemoryRegions<'a> {
+    regions: core::slice::Iter<'a, MemoryRegion>,
+}
+
+impl<'a> Iterator for MemoryRegions<'a> {
+    type Item = MemoryRegion;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut current = *self.regions.next()?;
+        loop {
+            #[allow(clippy::suspicious_operation_groupings)]
+            match self.regions.as_slice().get(0) {
+                Some(next) if current.kind == next.kind && current.end == next.start => {
+                    current.end = next.end;
+                    let _ = self.regions.next();
+                    continue;
+                }
+                _ => return Some(current),
+            }
+        }
+    }
+}
 
 entry_point!(kernel_main);
 
@@ -29,6 +55,18 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     desktop::draw().expect("failed to draw desktop");
 
     println!("Welcome to sabios!");
+
+    for region in (MemoryRegions {
+        regions: boot_info.memory_regions.iter(),
+    }) {
+        println!(
+            "addr={:08x}-{:08x}, pages = {:08x}, kind = {:?}",
+            region.start,
+            region.end,
+            (region.end - region.start) / 4096,
+            region.kind,
+        );
+    }
 
     mouse::draw_cursor().expect("failed to draw mouse cursor");
 

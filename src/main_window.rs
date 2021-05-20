765c61e7190d55545e747508ba6acd3f4be2dcf4
gifnksm <makoto.nksm+github@gmail.com>
2021-05-20 21:57:56 +0900

    Add fast loop counter (osbook_day10c)

--- src/main_window.rs
+++ src/main_window.rs
@@ -1,37 +1,75 @@
 use crate::{
     font,
-    graphics::{Color, Point, Size},
+    graphics::{Color, Draw, Point, Rectangle, Size},
     layer::{self, Layer},
     prelude::*,
     window::{self, Window},
 };
+use alloc::format;
+use core::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+struct Yield {
+    yielded: bool,
+}
+
+impl Yield {
+    fn new() -> Self {
+        Self { yielded: false }
+    }
+}
+
+impl Future for Yield {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if !self.yielded {
+            cx.waker().wake_by_ref();
+            self.yielded = true;
+            Poll::Pending
+        } else {
+            Poll::Ready(())
+        }
+    }
+}
 
 pub(crate) async fn handler_task() {
     let res = async {
-        let window = Window::new(Size::new(160, 68));
+        let window = Window::new(Size::new(160, 52));
 
         {
             let drawer = window.lock().drawer();
             let mut drawer = drawer.lock();
             window::draw_window(&mut *drawer, "Hello Window");
-            font::draw_str(&mut *drawer, Point::new(24, 28), "Welcome to", Color::BLACK);
-            font::draw_str(
-                &mut *drawer,
-                Point::new(24, 44),
-                " sabios world!",
-                Color::BLACK,
-            );
         }
 
         let mut layer = Layer::new();
         let layer_id = layer.id();
-        layer.set_window(Some(window));
+        layer.set_window(Some(window.clone()));
         layer.move_to(Point::new(300, 100));
 
         let tx = layer::event_tx();
         tx.register(layer)?;
         tx.set_height(layer_id, layer::MAIN_WINDOW_ID)?;
         tx.draw()?;
+
+        for count in 0.. {
+            {
+                let drawer = window.lock().drawer();
+                let mut drawer = drawer.lock();
+                drawer.fill_rect(
+                    Rectangle::new(Point::new(24, 28), Size::new(8 * 10, 16)),
+                    Color::from_code(0xc6c6c6),
+                );
+                let s = format!("{:010}", count);
+                font::draw_str(&mut *drawer, Point::new(24, 28), &s, Color::BLACK);
+                tx.draw()?;
+            }
+            Yield::new().await;
+        }
 
         Ok::<(), Error>(())
     }
